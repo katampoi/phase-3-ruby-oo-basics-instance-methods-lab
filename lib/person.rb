a44371ebdecd169ee3e8c1fcc03d1c600e6745c0
katampoi <simonkatampoi@student.moringaschool.com>
2022-08-21 11:26:01 +0300

    Complete Assignment

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+    def talk
+      puts "Hello World!"
+    end
+    def walk
+      puts "The Person is walking"
+    end
+  end
+  katampoi = Person.new
+  katampoi.talk
+  katampoi.walk
+  jack = Person.new
+  jack.talk
+  jack.walk
